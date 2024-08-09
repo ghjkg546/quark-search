d68b292c8705362a069f10268a3f3ee43527436e
546997008@qq.com <546997008@qq.com>
2024-08-09 15:47:40 +0800

    1.update crawl script 2.update README

--- README.md
+++ README.md
@@ -16,18 +16,21 @@
 ```
 cd main-search
 docker-compose up -d 
+mysql -P 9306 -h0
+CREATE TABLE resource_item (title string indexed stored, url string ,create_time integer )charset_table = 'non_cjk,chinese' morphology = 'icu_chinese';
+
 ```
 端口可以自己改，默认是9306，注意不要暴露9306到外网，以免遭受攻击，攻击者可能篡改，删除你的数据
 
 
 ### 爬取搜索数据
-在backend/crawl.py ,修改2处”夸克分享论坛url“为真实目标url，谷歌搜”夸克资源分享“，很容易找到那种一眼flarum搭建的，本脚本仅支持此类网站爬取
+在backend/crawl.py ,修改2处”baseUrl“为真实目标url，谷歌搜”夸克资源分享“，很容易找到那种一眼flarum搭建的，本脚本仅支持此类网站爬取
 time.sleep(),默认我是用了5秒，请自行修改间隔时间
 其余类型需自行修改脚本,同样上传到带有python环境的服务器运行，Manticore Search跟爬取脚本需要在同一台服务器，以方便从localhost投递数据
 
 ### 启动flask-api
 python api.py,启动即可，但这样关闭命令行后，服务端就断了。常驻后台需搭配supervisor运行,建议用1panel或者宝塔等面板运行，启动后验证下后端
-url能否打开，如访问http://服务器ip/api/data?page=1&keyword=夫妻你
+url能否打开，如访问http://服务器ip/api/data?page=1&keyword=父亲
 
 ### 编译前端静态文件
 如果没有pnpm请自行搜索并安装
--- backend/crawl.py
+++ backend/crawl.py
@@ -1,60 +1,59 @@
 import requests
-import re
 import json
 from bs4 import BeautifulSoup
 import time
-import re
 
 curTime = int(time.time())
 limit = 20
 
+baseUrl = "http://test.com"
+
+#提交数据到manti
+def postToManti(title,link):
+    url = "http://localhost:9308/sql"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "mode": "raw",
+        "query": f"INSERT INTO resource_item (title, url, create_time) VALUES ( '{title}','{link}','{curTime}');"
+    }
+    response = requests.post(url, headers=headers, data=data)
+    
+#获取页面内容
+def getPageContent(title,url1):
+    response = requests.get(url1)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    div = soup.find('div', class_='Post-body')
+    a_tags = div.find_all('a')
+    a_str =""
+    # 查找class为'Post-body'下的所有a标签，获取它们的href属性
+    for a in a_tags:
+        if a is not None:
+            linkUrl = a.get('href')
+            if(linkUrl is not None):
+                a_str+=linkUrl
+                print(a.get('href'))
+    if a_str != "":
+        postToManti(title,a_str)
+
 for i in range(2,1000):
     print("爬取接口"+str(limit*i))
     # 目标网页URL
-    baseUrl = '夸克分享论坛url='+str(limit*i)
+    baseUrl = baseUrl+'/api/discussions?include=user%2ClastPostedUser%2Ctags%2Ctags.parent%2CfirstPost&sort&page%5Boffset%5D='+str(limit*i)
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
     }
 
     response = requests.get(baseUrl,headers=headers)
     res = response.text
-    a = json.loads(res)
-    data =  a['data']
-
-    #提交数据到manti
-    def postToManti(title,link):
-        url = "http://localhost:9308/sql"
-        headers = {
-            'Content-Type': 'application/json'
-        }
-
-        data = {
-            "mode": "raw",
-            "query": f"INSERT INTO resource_item (title, url, create_time) VALUES ( '{title}','{link}','{curTime}');"
-        }
-        response = requests.post(url, headers=headers, data=data)
-        
-    #获取页面内容
-    def getPageContent(title,url1):
-        response = requests.get(url1)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        div = soup.find('div', class_='Post-body')
-        a_tags = div.find_all('a')
-        a_str =""
-        # 查找class为'Post-body'下的所有a标签，获取它们的href属性
-        for a in a_tags:
-            if a is not None:
-                linkUrl = a.get('href')
-                if(linkUrl is not None):
-                    a_str+=linkUrl
-                    print(a.get('href'))
-        if a_str != "":
-            postToManti(title,a_str)
-            #exit()
+    res_content = json.loads(res)
+    data = res_content['data']
 
     for b in data:
         print(b['attributes']['title'])
-        detail_url = "夸克分享论坛url"+b['id']
+        detail_url = baseUrl+"/d/"+b['id']
         getPageContent(b['attributes']['title'],detail_url)
         time.sleep(5)
         
